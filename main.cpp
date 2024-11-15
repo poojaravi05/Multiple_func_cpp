44d458ea44da3c4695493a9745e290c4cc7d0fdf
poojaravi05 <46893746+poojaravi05@users.noreply.github.com>
2024-11-15 11:37:18 -0800

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "func.h"
+
+int main() {
+  
+  std::cout << is_palindrome("noon") << "\n";
+  std::cout << two_power(4) << "\n";
+  std::cout << average(4.0, 7.0) << "\n";
+  
+}
